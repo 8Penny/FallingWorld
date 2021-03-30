8ff718c3a470b286cd4ac104a29454fa4cad2728
Polina <p.makarova@ll-games.com>
2021-03-30 17:34:35 +0300

    Added project

---
+++ Platforms/Assets/Scripts/Foundation/Managers/PlayerManager/IPlayer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Foundation
+{
+    public interface IPlayer
+    {
+        int Index { get; }
+        ICharacterHealth Health { get; }
+        ICharacterAgent Agent { get; }
+        IInventory Inventory { get; } 
+        Sprite Portrait { get; }
+        Vector3 Position { get; }
+    }
+}
